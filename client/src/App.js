0cad47c50ad6e5165923e94201fe01a3ea703d10
Shiv Shankar Prasad <shivshankarkumar.pusa@gmail.com>
2020-09-30 10:08:59 +0530

    Changes and production deployment

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/home';
 import NotFound from './components/NotFound/NotFound';
+import Signup from './components/Signup/Signup';
+import Signin from './components/Signin/Signin';
+import About from './components/About/About';
+import Services from './components/Services/Services';
+import AdminRoutes from './components/Admin/AdminRoutes/AdminRoutes';
+import AdminDashboard from './components/Admin/AdminDashboard/Admin';
 
 function App() {
    return (
@@ -15,18 +21,21 @@ function App() {
                <main>
                   <Switch>
                      <Route exact path='/' component={Home} />
-                     {/* <Route exact path='/signUp' component={SignUp} />
-                     <Route exact path='/signIn' component={SignIn} /> */}
+                     <Route exact path='/signup' component={Signup} />
+                     <Route exact path='/login' component={Signin} />
+                     <Route exact path='/services' component={Services} />
+                     <Route exact path='/about' component={About} />
+                     {/* 
                      {/* <UserRoutes
                         exact
                         path='/user/dashboard'
                         component={UserDashboard}
-                     />
+                     /> */}
                      <AdminRoutes
                         exact
                         path='/admin/dashboard'
                         component={AdminDashboard}
-                     /> */}
+                     />
                      <Route component={NotFound} />
                   </Switch>
                </main>
--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import {
    FacebookIcon,
@@ -6,7 +6,6 @@ import {
    LinkedinIcon,
    LinkedinShareButton,
    TwitterShareButton,
-   EmailIcon,
    PinterestShareButton,
    PinterestIcon,
    TelegramShareButton,
@@ -14,6 +13,7 @@ import {
    TelegramIcon,
 } from 'react-share';
 import './index.css';
+import { isAuthenticated } from '../Helpers/auth';
 
 /**
  * @author
@@ -23,70 +23,107 @@ import './index.css';
 const Footer = () => {
    return (
       <footer>
-         <div className='main-footer'>
-            <div className='container'>
-               <div className='row contents'>
-                  <div className='col-md-4'>
-                     <ul className='unordered-list'>
-                        <h4>New Item </h4>
-                        <Link to='' className='footer-links'>
-                           <i className='fab fa-facebook'></i>
-                        </Link>
-                     </ul>
-                  </div>
+         {!isAuthenticated() && (
+            <Fragment>
+               <div className='main-footer'>
+                  <div className='container'>
+                     <div className='row contents'>
+                        <div className='col-md-4'>
+                           <ul className='unordered-list'>
+                              <h4>New Item </h4>
+                              <Link to='' className='footer-links'>
+                                 <i className='fab fa-facebook'></i>
+                              </Link>
+                           </ul>
+                        </div>
 
-                  <div className='col-md-4'>
-                     <ul className='Other Stuff'>
-                        <h4>Terms </h4>
-                        <Link to='' className='footer-links'>
-                           <i className='fab fa-facebook'></i>
-                        </Link>
-                     </ul>
-                  </div>
+                        <div className='col-md-4'>
+                           <ul className='Other Stuff'>
+                              <h4>Terms </h4>
+                              <Link to='' className='footer-links'>
+                                 <i className='fab fa-facebook'></i>
+                              </Link>
+                           </ul>
+                        </div>
 
-                  <div className='col-md-4'>
-                     <ul className='social-media-icons'>
-                        <div className='footer-links'>
-                           <FacebookShareButton
-                              url={
-                                 'https://www.facebook.com/shivshankarkumar.pusa/'
-                              }
+                        <div className='col-md-4'>
+                           <ul className='social-media-icons'>
+                              <div className='footer-links'>
+                                 <FacebookShareButton
+                                    url={
+                                       'https://www.facebook.com/shivshankarkumar.pusa/'
+                                    }
+                                 >
+                                    <FacebookIcon size={32} round={true} />
+                                 </FacebookShareButton>
+                              </div>
+                              <div className='footer-links'>
+                                 <TelegramShareButton>
+                                    <TelegramIcon size={32} round={true} />
+                                 </TelegramShareButton>
+                              </div>
+                              <div className='footer-links'>
+                                 <LinkedinShareButton>
+                                    <LinkedinIcon size={32} round={true} />
+                                 </LinkedinShareButton>
+                              </div>
+                              <div className='footer-links'>
+                                 <TwitterShareButton>
+                                    <TwitterIcon size={32} round={true} />
+                                 </TwitterShareButton>
+                              </div>
+                              <div className='footer-links'>
+                                 <PinterestShareButton>
+                                    <PinterestIcon size={32} round={true} />
+                                 </PinterestShareButton>
+                              </div>
+                           </ul>
+                        </div>
+                     </div>
+                     <div className='row'>
+                        <p className='col-sm privacy-policy'>
+                           {' '}
+                           &copy; {new Date().getFullYear()} This Corporation |
+                           All Rights reserved |{' '}
+                           <Link
+                              to='/terms-of-condition'
+                              className='terms-condition-link'
                            >
-                              <FacebookIcon size={32} round={true} />
-                           </FacebookShareButton>
-                        </div>
-                        <div className='footer-links'>
-                           <TelegramShareButton>
-                              <TelegramIcon size={32} round={true} />
-                           </TelegramShareButton>
-                        </div>
-                        <div className='footer-links'>
-                           <LinkedinShareButton>
-                              <LinkedinIcon size={32} round={true} />
-                           </LinkedinShareButton>
-                        </div>
-                        <div className='footer-links'>
-                           <TwitterShareButton>
-                              <TwitterIcon size={32} round={true} />
-                           </TwitterShareButton>
-                        </div>
-                        <div className='footer-links'>
-                           <PinterestShareButton>
-                              <PinterestIcon size={32} round={true} />
-                           </PinterestShareButton>
-                        </div>
-                     </ul>
+                              Terms of Services
+                           </Link>{' '}
+                           |{' '}
+                           <Link to='/privacy' className='privacy-policy-link'>
+                              Privacy Policy
+                           </Link>
+                        </p>
+                     </div>
                   </div>
                </div>
-               <div className='row'>
-                  <p className='col-sm privacy-policy'>
-                     {' '}
-                     &copy; {new Date().getFullYear()} This Corporation|All
-                     Right reserved | Terms of Services | Privacy Policy
-                  </p>
-               </div>
-            </div>
-         </div>
+            </Fragment>
+         )}
+         {isAuthenticated() && isAuthenticated().role === 0 && (
+            <Fragment>
+               <footer>
+                  <div className='row'>
+                     <p className='col-sm privacy-policy'>
+                        {' '}
+                        &copy; {new Date().getFullYear()} This Corporation | All
+                        Rights reserved |{' '}
+                        <Link
+                           to='/terms-of-condition'
+                           className='terms-condition-link'
+                        >
+                           Terms of Services
+                        </Link>{' '}
+                        |{' '}
+                        <Link to='/privacy' className='privacy-policy-link'>
+                           Privacy Policy
+                        </Link>
+                     </p>
+                  </div>
+               </footer>
+            </Fragment>
+         )}
       </footer>
    );
 };
